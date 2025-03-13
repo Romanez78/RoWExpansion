8f1db8ecf13e2fd35b24c3d59710a302538cb941
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2025-03-14 00:52:13 +0100

    Update CronosROW.d

--- NINJA/ROWEXPANSION/CONTENT/Dialogs/CronosROW.d
+++ NINJA/ROWEXPANSION/CONTENT/Dialogs/CronosROW.d
@@ -24,6 +24,6 @@ func void Ninja_RoWExpansion_DIA_CRONOS_GIVEARMORINDEED_Info ()
     AI_Output(other, self, Ninja_RoWExpansion_DIA_CRONOS_GIVEARMORINDEED_15_01); //Saturas říkal že pro mně něco máš.
 	AI_Output(self, other, Ninja_RoWExpansion_DIA_CRONOS_GIVEARMORINDEED_15_02); //Ano, mám tu pro tebe lepší zbroj. Nechceme ztratit dalšiho člena kruhu. 
     AI_Output(other, self, Ninja_RoWExpansion_DIA_CRONOS_GIVEARMORINDEED_13_03); //Díky, bude se hodit.
-    B_GiveInvItems(self, other, ITAR_RNG_H, 1);
+    B_GiveInvItems(self, other, ITAR_RNG_H_RoWExpansion, 1);
 	Ninja_RoWExpansion_DIA_Cronos_ArmorKaufen_perm = TRUE;	
 };
