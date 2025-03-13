7fcfd2b8cbd57cc4b01186326f5772b4402572a5
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2025-03-14 00:51:57 +0100

    Update AngarROW.d

--- NINJA/ROWEXPANSION/CONTENT/Dialogs/AngarROW.d
+++ NINJA/ROWEXPANSION/CONTENT/Dialogs/AngarROW.d
@@ -13,7 +13,7 @@ instance Ninja_RoWExpansion_DIA_Angar_HelpMeImproveArmor (C_INFO)
 func int Ninja_RoWExpansion_DIA_Angar_HelpMeImproveArmor_Condition ()
 {	
 	if (DJG_AngarGotAmulett == FALSE)
-	&& (Npc_HasItems(other, ItAr_RNG_TM) > 0)
+	&& (Npc_HasItems(other, ItAr_RNG_TM_RoWExpansion) > 0)
 	{
 		return TRUE;
 	};
@@ -38,7 +38,7 @@ instance Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet (C_INFO)
 func int Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_Condition ()
 {	
 	if (DJG_AngarGotAmulett == TRUE)
-	&& (Npc_HasItems(other, ItAr_RNG_TM) > 0)
+	&& (Npc_HasItems(other, ItAr_RNG_TM_RoWExpansion) > 0)
 	{
 		return TRUE;
 	};
@@ -47,13 +47,13 @@ func void Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_Info ()
 {
     AI_Output(other, self, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_01); //Nemohl by ses mi podívat na tuhle zbroj? 
 	AI_Output(self, other, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_02); //Ukaž mi ji. 
-	B_GiveInvItems(other, self, ITAR_RNG_TM, 1);
+	B_GiveInvItems(other, self, ITAR_RNG_TM_RoWExpansion, 1);
 	AI_Output(self, other, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_03); //Zajímavá, podobá se té kterou nosím já. Co chceš abych s ní udělal?
 	AI_Output(other, self, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_04); //Nemohl by si mi jí vylepšit? 
 	AI_Output(other, self, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_05); //Napadlo mně se zeptat tebe, jelikož se podobá té kterou nosíš.
 	AI_Output(self, other, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_06); //Určitě, bude to snadné.
 	AI_Output(self, other, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_07); //Tady, tohle je co sis představoval ne?
-	B_GiveInvItems(self, other, ITAR_RNG_TH, 1);
+	B_GiveInvItems(self, other, ITAR_RNG_TH_RoWExpansion, 1);
 	AI_Output(other, self, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_08); //Díky, co ti za to dlužím?
 	AI_Output(self, other, Ninja_RoWExpansion_DIA_Angar_NowYouHaveTheAmulet_15_09); //Nech to být, pomohl si mi najít amulet, to mi stačí.
 };
